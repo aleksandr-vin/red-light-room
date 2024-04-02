999dd13250492fe0a82bf2857aff9e91ac6ed7c3
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2024-04-02 22:55:11 +0200

    Initial Commit

---
+++ Red Light Room/Red_Light_RoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Red_Light_RoomApp.swift
+//  Red Light Room
+//
+//  Created by Aleksandr Vinokurov on 02-04-2024.
+//
+
+import SwiftUI
+
+@main
+struct Red_Light_RoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
